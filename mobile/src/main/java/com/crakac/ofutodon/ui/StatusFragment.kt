e723bbef21cff0844b13052d18acb2fe6e928b0c
Kosuke Shirakashi <k.shira86@gmail.com>
2017-07-22 21:23:23 +0900

    タブのelevationをいい感じにした

--- mobile/src/main/java/com/crakac/ofutodon/ui/StatusFragment.kt
+++ mobile/src/main/java/com/crakac/ofutodon/ui/StatusFragment.kt
@@ -41,7 +41,7 @@ class StatusFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener, Mastodo
 
     var streaming: MastodonStreaming? = null
 
-    private var isLoadingNext = false;
+    private var isLoadingNext = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_status, container, false)
