f2ef5ee519fea9e4e17709c87e3c0564b6bc5ef2
Kosuke Shirakashi <k.shira86@gmail.com>
2017-08-26 17:42:01 +0900

    ブーストされたトゥートのアイコンをタップした時に発言元の人のアカウントに飛ぶようにした

--- mobile/src/main/java/com/crakac/ofutodon/ui/StatusFragment.kt
+++ mobile/src/main/java/com/crakac/ofutodon/ui/StatusFragment.kt
@@ -196,7 +196,7 @@ abstract class StatusFragment : Fragment(),
 
     override fun onIconClicked(icon: ImageView, status: Status) {
         val intent = Intent(activity, UserActivity::class.java)
-        UserActivity.setUserInfo(intent, status.account)
+        UserActivity.setUserInfo(intent, status.reblog?.account ?: status.account)
         val options = ActivityOptions.makeSceneTransitionAnimation(activity, icon, getString(R.string.transition_name_user_icon))
         startActivity(intent, options.toBundle())
     }
