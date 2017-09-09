14610a5b455375816b16dfe85c39604e988328b3
Kosuke Shirakashi <k.shira86@gmail.com>
2017-09-09 17:21:32 +0900

    WIP ConversationActivity

---
+++ mobile/src/main/java/com/crakac/ofutodon/ui/ConversationFragment.kt
@@ -0,0 +1,21 @@
+package com.crakac.ofutodon.ui
+
+import com.crakac.ofutodon.R
+import com.crakac.ofutodon.model.api.entity.Status
+import retrofit2.Call
+
+class ConversationFragment(): StatusFragment() {
+    val TAG: String = "ConversationFragment"
+
+
+
+    override fun onRefreshRequest(): Call<List<Status>>? {
+        return null
+    }
+
+    override fun onLoadMoreRequest(): Call<List<Status>>? {
+        return null
+    }
+
+    override fun getTitle(): String = getString(R.string.conversation)
+}
